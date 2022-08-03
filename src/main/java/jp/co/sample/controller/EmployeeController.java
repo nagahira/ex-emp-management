921c8f63dc068feaee05ab400655e179a1837586
hiratanagahiro <hiratanagahiro@gmail.com>
2022-08-03 16:58:03 +0900

    detail.html動作未確認 ease enter the commit message for your changes. Lines starting

--- src/main/java/jp/co/sample/controller/EmployeeController.java
+++ src/main/java/jp/co/sample/controller/EmployeeController.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import jp.co.sample.domain.Employee;
+import jp.co.sample.form.UpdateEmployeeForm;
 import jp.co.sample.service.EmployeeService;
 
 
@@ -28,4 +30,21 @@ public class EmployeeController {
 		model.addAttribute("employeeList",employeeList);
 		return "employee/list.html";
 	}
+	
+	@ModelAttribute
+	public UpdateEmployeeForm setUpUpdateEmployeeForm() {
+		UpdateEmployeeForm updateEmployeeForm = new UpdateEmployeeForm();
+		return updateEmployeeForm;
+	}
+	
+	@RequestMapping("/showDetail")
+	public String showDetail(String id,Model model) {
+		int empId = Integer.parseInt(id);
+		Employee employee = new Employee();
+		employee = employeeService.showDetail(empId);
+		model.addAttribute("employee",employee);
+		return "employee/detail.html";
+	}
 }
+
+

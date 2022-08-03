5816a309542b58fca9e9d4730b9f2c49dc4ffdb7
hiratanagahiro <hiratanagahiro@gmail.com>
2022-08-03 15:05:47 +0900

    UpdateEmployeeFormの作成。

---
+++ src/main/java/jp/co/sample/form/UpdateEmployeeForm.java
@@ -0,0 +1,31 @@
+package jp.co.sample.form;
+
+/**
+ * employeeアップデート用フォームの定義
+ * @author hiratanagahiro
+ *
+ */
+
+public class UpdateEmployeeForm {
+private String id;
+private String dependentsCount;
+
+public String getId() {
+	return id;
+}
+public void setId(String id) {
+	this.id = id;
+}
+public String getDependentsCount() {
+	return dependentsCount;
+}
+public void setDependentsCount(String dependentsCount) {
+	this.dependentsCount = dependentsCount;
+}
+@Override
+public String toString() {
+	return "UpdateEmployeeForm [id=" + id + ", dependentsCount=" + dependentsCount + "]";
+}
+
+
+}
